dccf8d6468a17ed62c4e12155f98185eb399b636
Tyler Ross <tross020890@gmail.com>
2022-06-13 12:31:46 -0500

    Feat: returns all results if less than specified quantity

--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe BookFacade do
     expect(result).to eq(42475)
   end
 
+  it 'returns all search results if total is less than quantity', :vcr do
+
+    response_body = File.read("spec/fixtures/few_results.json")
+      stub_request(:get, "http://openlibrary.org/search.json?q=clarendon,tx").
+        with(
+          headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent'=>'Faraday v2.3.0'
+          }).
+          to_return(status: 200, body: response_body, headers: {})
+    result = BookFacade.search_books("clarendon,tx", 5)
+    expect(result.count).to eq(4)
+  end
+
 end
