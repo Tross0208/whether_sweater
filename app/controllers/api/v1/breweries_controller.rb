c5b0dd27050773e9ee982e1ace6ecc4e9e84c824
Tyler Ross <tross020890@gmail.com>
2022-06-14 12:16:06 -0500

    feat:add sad path for bad query params)

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::BreweriesController < ApplicationController
   def index
-    location = MapFacade.get_coords(params[:location])
-    weather = WeatherFacade.get_weather(location)
-    breweries = BreweryFacade.total_results(params[:location], params[:quantity])
-    render json: BrewerySerializer.brewery_index_serializer(breweries, params[:location], weather)
+    if params[:location] && (params[:quantity].to_i > 0)
+      location = MapFacade.get_coords(params[:location])
+      weather = WeatherFacade.get_weather(location)
+      breweries = BreweryFacade.total_results(params[:location], params[:quantity])
+      render json: BrewerySerializer.brewery_index_serializer(breweries, params[:location], weather)
+    else
+      render status: 404
+    end
   end
+
 end
--- spec/requests/api/v1/brewery_request_spec.rb
+++ spec/requests/api/v1/brewery_request_spec.rb
@@ -3,7 +3,17 @@ require 'rails_helper'
 RSpec.describe "Brewery API request" do
   it 'returns serialized breweries', :vcr do
     response = get "/api/v1/breweries?location=denver&quantity=5"
-    
+
     expect(response).to eq(200)
+  end
+  it 'denies request with no location', :vcr do
+    response = get "/api/v1/breweries?quantity=5"
+
+    expect(response).to eq(404)
+  end
+  it 'denies request with invalid quantity', :vcr do
+    response = get "/api/v1/breweries?location=denver&quantity=0"
+
+    expect(response).to eq(404)
   end
 end
